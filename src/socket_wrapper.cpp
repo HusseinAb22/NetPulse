72134b47e4fe50a59cfb7639d23c3f45aac2cf16
HusseinAb22 <hussein.ab222@gmail.com>
2026-04-28 14:21:52 +0300

    implement SocketWrapper RAII class for safe file descriptor management

--- src/socket_wrapper.cpp
+++ src/socket_wrapper.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "../include/socket_wrapper.h"
+
+SocketWrapper::SocketWrapper() {
+
+}
+SocketWrapper::~SocketWrapper() {
+    if (this->fd_!=-1) {
+        close(fd_);
+    }
+}
+
+SocketWrapper::SocketWrapper(const int fd) : fd_(fd) {
+
+}
+
+SocketWrapper::SocketWrapper(SocketWrapper &&other) noexcept :fd_(std::exchange(other.fd_, -1)){
+
+}
+
+SocketWrapper& SocketWrapper::operator=(SocketWrapper &&other) noexcept {
+    if (this != &other) {
+        SocketWrapper temp(std::move(other));
+        std::swap(this->fd_, temp.fd_);
+    }
+    return *this;
+}
+
+int SocketWrapper::getFd() const {
+    return fd_;
+}
